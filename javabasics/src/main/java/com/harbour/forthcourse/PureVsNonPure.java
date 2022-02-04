b714c26ad5d76778a3e738a9e93717e03c64b1e1
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2022-02-04 18:04:59 +0100

    Demo only

---
+++ javabasics/src/main/java/com/harbour/forthcourse/PureVsNonPure.java
@@ -0,0 +1,30 @@
+package com.harbour.forthcourse;
+public class PureVsNonPure {
+    int counter = 0;
+    public int functionA(int a, int b) {
+        return a + b;
+    }
+    public int functionB(int a) {
+        this.counter = this.counter + a;
+        return this.counter;
+    }
+    public static void main(String[] args) {
+        final var pureVsNonPure = new PureVsNonPure();
+
+        final var i = pureVsNonPure.functionA(2, 2);
+        final var i2 = pureVsNonPure.functionA(2, 2);
+        final var i3 = pureVsNonPure.functionA(2, 2);
+        System.out.println(i);
+        System.out.println(i2);
+        System.out.println(i3);
+
+        final var i4 = pureVsNonPure.functionB(1);
+        final var i5 = pureVsNonPure.functionB(1);
+        final var i6 = pureVsNonPure.functionB(1);
+
+        System.out.println(i4);
+        System.out.println(i5);
+        System.out.println(i6);
+
+    }
+}
