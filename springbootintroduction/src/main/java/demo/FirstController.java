db991d5baadea17aecfb5482cd4f44bc524ea8d5
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2022-02-08 15:10:03 +0100

    Add rest controller

---
+++ springbootintroduction/src/main/java/demo/FirstController.java
@@ -0,0 +1,18 @@
+package demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FirstController {
+
+    @Autowired
+    @Qualifier("Vasya") Lector lector;
+
+    @GetMapping("/hello")
+    public Lector get() {
+        return lector;
+    }
+}
