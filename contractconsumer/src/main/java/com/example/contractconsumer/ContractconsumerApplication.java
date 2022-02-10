f1a76e63525018c61f6d5db14d87a506730a8444
Igor Sakhankov <igor.sakhankov@glovoapp.com>
2022-02-10 16:11:52 +0100

    Add missing files

---
+++ contractconsumer/src/main/java/com/example/contractconsumer/ContractconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.example.contractconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContractconsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContractconsumerApplication.class, args);
+    }
+
+}
